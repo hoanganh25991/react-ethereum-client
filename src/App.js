6c5b2d6c9277a8a84fcde8539b49b3c226165a7e
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-01 09:30:16 +0700

    Try toUnixTime ^^

--- src/App.js
+++ src/App.js
@@ -1,194 +1,111 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FD_NewPolicyJson from "./built-contracts/FlightDelayNewPolicy.json"
+
+const _ = console.log
 
 class App extends Component {
   constructor(props){
     super(props);
-    const MyContract =  window.web3.eth.contract([
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "you_awesome",
-        "outputs": [
-          {
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "pseudoRandomResult",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "getState",
-        "outputs": [
-          {
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "getSecret",
-        "outputs": [
-          {
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": false,
-            "name": "result",
-            "type": "bool"
-          }
-        ],
-        "name": "ExperimentComplete",
-        "type": "event"
-      },
-      {
-        "constant": false,
-        "inputs": [],
-        "name": "kill",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [],
-        "name": "setExperimentInMotion",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "newState",
-            "type": "string"
-          }
-        ],
-        "name": "setState",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-      },
-      {
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "fallback"
-      }
-    ]);
+    const {abi} = FD_NewPolicyJson;
+    const FD_NewPolicy =  window.web3.eth.contract(abi);
 
     this.state = {
-      ContractInstance: MyContract.at("0xf70e33ea24c8018602f61efc61909e0ad0488c55"),
-      contractState: '',
+      FD_NewPolicy: FD_NewPolicy.at("0x1b3a3ad2137df3b141e483884ee7181372851ced"),
     }
-
-    this.state.event = this.state.ContractInstance.ExperimentComplete();
+    // this.state.event = this.state.FD_NewPolicy.ExperimentComplete();
   }
 
-  querySecret = () => {
-    const {ContractInstance} = this.state
-    ContractInstance.getSecret((err, secret) => {
-      if(err) console.error('An error occured:::', err)
-      console.log("This is our contract's secret:::", secret);
-    })
-  }
+  // querySecret = () => {
+  //   const {FD_NewPolicy} = this.state
+  //   FD_NewPolicy.getSecret((err, secret) => {
+  //     if(err) console.error('An error occured:::', err)
+  //     console.log("This is our contract's secret:::", secret);
+  //   })
+  // }
+  //
+  // queryContractState = () => {
+  //   const {FD_NewPolicy} = this.state;
+  //   FD_NewPolicy.getState((err, state) => {
+  //     if(err) console.error('An error occured:::', err)
+  //     console.log("This is our contract's state:::", state)
+  //   })
+  // }
+  //
+  // handleContractStateSubmit = (event) => {
+  //   event.preventDefault();
+  //
+  //   const {FD_NewPolicy, contractState: newState} = this.state;
+  //   FD_NewPolicy.setState(newState, {
+  //       gas: 300000,
+  //       from: window.web3.eth.accounts[0],
+  //       value: window.web3.toWei(0.01, 'ether')
+  //     }, (err, result) => {
+  //       if(err) console.error('[setState][ERR]', err)
+  //       console.log('Smart contract state is changing', result)
+  //     })
+  // }
+  //
+  // queryConditionalResult = () => {
+  //   const {FD_NewPolicy} = this.state;
+  //   FD_NewPolicy.pseudoRandomResult((err, result) => {
+  //     if(err) console.error("[pseudoRandomResult][ERR]", err)
+  //     console.log("This is our contract's pseudoRandomResult:::", result)
+  //   })
+  // }
+  //
+  // activateExperiment = () => {
+  //   const {FD_NewPolicy} = this.state
+  //   FD_NewPolicy.setExperimentInMotion({
+  //     gas: 300000,
+  //     from: window.web3.eth.accounts[0],
+  //     value: window.web3.toWei(0.01, 'ether')
+  //   }, (err, result) => {
+  //     if (err) console.error("[setExperimentInMotion][ERR]", err)
+  //     console.log("[activateExperiment]", result)
+  //   })
+  // }
 
-  queryContractState = () => {
-    const {ContractInstance} = this.state;
-    ContractInstance.getState((err, state) => {
-      if(err) console.error('An error occured:::', err)
-      console.log("This is our contract's state:::", state)
-    })
-  }
+  toUnixTime = () => {
+    const web3 = window.web3;
+    const acc1 = web3.eth.accounts[0];
+    const {FD_NewPolicy} = this.state;
+    _("[FD_NewPolicy]", FD_NewPolicy);
 
-  handleContractStateSubmit = (event) => {
-    event.preventDefault();
-
-    const {ContractInstance, contractState: newState} = this.state;
-    ContractInstance.setState(newState, {
-        gas: 300000,
-        from: window.web3.eth.accounts[0],
-        value: window.web3.toWei(0.01, 'ether')
-      }, (err, result) => {
-        if(err) console.error('[setState][ERR]', err)
-        console.log('Smart contract state is changing', result)
-      })
-  }
-
-  queryConditionalResult = () => {
-    const {ContractInstance} = this.state;
-    ContractInstance.pseudoRandomResult((err, result) => {
-      if(err) console.error("[pseudoRandomResult][ERR]", err)
-      console.log("This is our contract's pseudoRandomResult:::", result)
-    })
-  }
-
-  activateExperiment = () => {
-    const {ContractInstance} = this.state
-    ContractInstance.setExperimentInMotion({
-      gas: 300000,
-      from: window.web3.eth.accounts[0],
-      value: window.web3.toWei(0.01, 'ether')
+    FD_NewPolicy.toUnixtime("0x2f6465702f323031382f30322f3232", {
+      gas: 600000,
+      from: acc1,
     }, (err, result) => {
-      if (err) console.error("[setExperimentInMotion][ERR]", err)
-      console.log("[activateExperiment]", result)
+      if(err) return _(`${err}`)
+      return _(`[i.toUnixtime][result][rawStr]`, result, result.toString());
     })
+  }
+
+  createNewPolicy = () => {
+    const web3 = window.web3;
+
+    const {FD_NewPolicy} = this.state;
+
+    _("[FD_NewPolicy]", FD_NewPolicy);
+
+    // FD_NewPolicy.newPolicy(1,2,3,4,5, {
+    //   gas: 600000,
+    //   from: web3.eth.accounts[0],
+    //   value: web3.toWei(0.01, 'ether')
+    // }, (err, result) => {
+    //   if(err) return _(`${err}`)
+    //   return _(result)
+    // })
   }
 
   render() {
-    const {event} = this.state
-
-    event.watch((err, result) => {
-      if(err) console.error("[experimentEvent][ERR]", err)
-      console.log("[experimentEvent]", result);
-    })
+    // const {event} = this.state
+    //
+    // event.watch((err, result) => {
+    //   if(err) console.error("[experimentEvent][ERR]", err)
+    //   console.log("[experimentEvent]", result);
+    // })
 
     return (
       <div className="App">
@@ -198,28 +115,29 @@ class App extends Component {
         </header>
         <br/>
         <br/>
-        <button onClick={this.querySecret}>Query Smart Contract's Secret</button>
-        <br/>
-        <br/>
-        <button onClick={this.queryContractState}>Query Smart Contract's State</button>
-        <br/>
-        <br/>
-        <form onSubmit={this.handleContractStateSubmit}>
-          <input
-            type={"text"}
-            name={"state-change"}
-            placeholder={"Enter new state..."}
-            value={this.state.contractState}
-            onChange={e => this.setState({contractState: e.target.value})}
-          />
-          <button type={"submit"}>Submit</button>
-        </form>
+        {/*<button onClick={this.querySecret}>Query Smart Contract's Secret</button>*/}
         {/*<br/>*/}
-        <br/>
-        <button onClick={this.queryConditionalResult}>Query Smart Contract Conditional Result</button>
-        <br/>
-        <br/>
-        <button onClick={this.activateExperiment}>Start Experiment On Smart Contract</button>
+        {/*<br/>*/}
+        {/*<button onClick={this.queryContractState}>Query Smart Contract's State</button>*/}
+        {/*<br/>*/}
+        {/*<br/>*/}
+        {/*<form onSubmit={this.handleContractStateSubmit}>*/}
+          {/*<input*/}
+            {/*type={"text"}*/}
+            {/*name={"state-change"}*/}
+            {/*placeholder={"Enter new state..."}*/}
+            {/*value={this.state.contractState}*/}
+            {/*onChange={e => this.setState({contractState: e.target.value})}*/}
+          {/*/>*/}
+          {/*<button type={"submit"}>Submit</button>*/}
+        {/*</form>*/}
+        {/*<br/>*/}
+        {/*<button onClick={this.queryConditionalResult}>Query Smart Contract Conditional Result</button>*/}
+        {/*<br/>*/}
+        {/*<br/>*/}
+        {/*<button onClick={this.activateExperiment}>Start Experiment On Smart Contract</button>*/}
+        <button onClick={this.toUnixTime}>To Unix Time</button>
+        <button onClick={this.createNewPolicy}>Create New Policy</button>
       </div>
     );
   }
